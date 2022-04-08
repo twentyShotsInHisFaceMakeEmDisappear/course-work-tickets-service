8f0e63fdd32032410bd8f13994d975b49444b102
maksimsamsonovich <maksim_samsanovich@senla.eu>
2022-04-08 16:36:18 +0300

    some changes

--- src/main/resources/db/changelog/homework-2/sql/27.artists-table-data-insert.sql
+++ src/main/resources/db/changelog/homework-2/sql/27.artists-table-data-insert.sql
@@ -1,4 +1,17 @@
 insert into artists
     ( users_id, nickname, avatar, short_quote, long_quote )
-values ( 1,  'JuiceWRLD', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2019_49/3138951/191208-juice-wlrd-mn-1010.jpg',
-        'Everybody stay positive no matter how negative life gets.', 'A lot of men make fun of you for expressing your feelings because they don’t get it. They can’t wrap their heads around the concept of being completely honest and forthright.')
+values ( 1,  'JuiceWRLD', 'https://cdns-images.dzcdn.net/images/artist/32458864045f1087652738e10bcdf74b/500x500.jpg',
+        'Everybody stay positive no matter how negative life gets.',
+        'A lot of men make fun of you for expressing your feelings because they don’t get it. They can’t wrap their heads around the concept of being completely honest and forthright.');
+
+insert into artists
+( users_id, nickname, avatar, short_quote, long_quote )
+values ( 1,  'Kid Cudi', 'https://i1.sndcdn.com/artworks-0ZByyWlOtNChod6W-xpWc3A-t500x500.jpg',
+         'Im an emotional creature. I make music to express that.',
+        'You can always say sorry but the real apology is when you hear the sadness in their voice and see the look in their eyes. And you realize that they have hurt themselves just as much.');
+
+insert into artists
+( users_id, nickname, avatar, short_quote, long_quote )
+values ( 1,  'Lil Skies', 'https://i1.sndcdn.com/avatars-YWz3wxKgSye2Niqs-T4Btyg-t500x500.jpg',
+         'I will carry all your names and I will carry all your shame.',
+        'We used to playing pretend, giving each other different names, we would build a rocket ship and then we’d fly it far away, used to dream of outer space but now they’re laughing in our face, saying, ''wake up, you need to make money''.');
--- src/main/resources/db/changelog/homework-2/sql/28.events-table-data-insert.sql
+++ src/main/resources/db/changelog/homework-2/sql/28.events-table-data-insert.sql
@@ -17,4 +17,46 @@ values (
         18,
         '2021-12-16',
         '3:00:00',
-        150 )
+        150 );
+
+insert into events (
+    title,
+    artists_id,
+    locations_id,
+    description,
+    occupied_places,
+    age_limit,
+    date,
+    continuance,
+    price )
+values (
+           'Man On The Moon',
+           2,
+           1,
+           'I will me fine, i get it. I will be coooooooool hmmmm.',
+           23,
+           21,
+           '2022-11-06',
+           '3:00:00',
+           150 );
+
+insert into events (
+    title,
+    artists_id,
+    locations_id,
+    description,
+    occupied_places,
+    age_limit,
+    date,
+    continuance,
+    price )
+values (
+           'OPS WANT ME DEAD',
+           3,
+           1,
+           'Live your life like we die in the world space. Fell so right.',
+           15,
+           18,
+           '2021-12-16',
+           '3:00:00',
+           150 );
