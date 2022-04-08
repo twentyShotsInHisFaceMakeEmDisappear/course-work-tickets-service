f538dcf379243181eced1be0fa15a929396b02a0
maksimsamsonovich <maksim_samsanovich@senla.eu>
2022-04-08 16:39:28 +0300

    some changes

--- src/main/java/eu/senla/eventoservice/exception/event/EventNotFoundException.java
+++ src/main/java/eu/senla/eventoservice/exception/event/EventNotFoundException.java
@@ -2,6 +2,7 @@ package eu.senla.eventoservice.exception.event;
 
 public class EventNotFoundException extends RuntimeException {
 
+    // Event not found Runtime Exception
     public EventNotFoundException(String errorMessage) {
         super(errorMessage);
     }
